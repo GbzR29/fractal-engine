634cbbaa0373a6ab5a53cfa71f69e0cfce378068
Su4amp <gabrielfeliperc@hotmail.com>
2026-03-25 20:37:39 -0300

    refatorado absolutamente tudo

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/gabri/Desktop/fractal-engine v2.0
+# Install script for directory: C:/Projetos/Github Projects/fractal-engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,13 +39,13 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/gabri/Desktop/fractal-engine v2.0/build/src/cmake_install.cmake")
+  include("C:/Projetos/Github Projects/fractal-engine/build/src/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/gabri/Desktop/fractal-engine v2.0/build/install_local_manifest.txt"
+  file(WRITE "C:/Projetos/Github Projects/fractal-engine/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/gabri/Desktop/fractal-engine v2.0/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Projetos/Github Projects/fractal-engine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
